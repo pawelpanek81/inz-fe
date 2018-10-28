1752bd69c3713e7c06f79093283397edb6980a9a
Paweł P <panczo12d@gmail.com>
2018-10-28 11:03:38 +0100

    add profile page

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,6 +1,7 @@
 export default {
   LOGIN: '/auth/login',
   REGISTER: '/account/users',
+  USER_INFO: '/account/users/info',
   MAP: '/map/map-points',
   MAP_POINT_TYPES: '/map/map-point-types',
 };
--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,9 +1,92 @@
 <template>
-  <div>
-    tutaj będzie profil
-    <div>imie</div>
-    <div>nazwisko</div>
-    <div>username</div>
-    <div>email</div>
-  </div>
+  <v-container fluid class="pa-2">
+    <v-layout wrap>
+      <v-flex xs12 sm6 class="pa-2">
+        <v-card>
+          <v-card-title primary-title>
+            <span class="title">Twoje konto</span>
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-layout wrap>
+                <v-flex>
+                  <v-text-field
+                    disabled
+                    v-model="user.name"
+                    prepend-icon="face"
+                    label="Imię">
+                  </v-text-field>
+                </v-flex>
+                <v-flex>
+                  <v-text-field
+                    disabled
+                    v-model="user.surname"
+                    prepend-icon="perm_identity"
+                    label="Nazwisko">
+                  </v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-text-field
+                disabled
+                v-model="user.username"
+                prepend-icon="account_circle"
+                label="Nazwa użytkownika">
+              </v-text-field>
+              <v-text-field
+                disabled
+                v-model="user.email"
+                prepend-icon="alternate_email"
+                label="Email">
+              </v-text-field>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm6 class="pa-2">
+        <v-img height="300px" src="/static/eugene-mckenna-1088650-unsplash.jpg"></v-img>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+import endpoints from '@/api/endpoints';
+import swal from 'sweetalert2';
+
+export default {
+  data() {
+    return {
+      user: {
+        name: '',
+        surname: '',
+        username: '',
+        email: '',
+      },
+    };
+  },
+  methods: {
+    fetchData() {
+      this.$http.get(endpoints.USER_INFO)
+        .then((response) => {
+          this.user = response.data;
+        })
+        .catch((error) => {
+          const code = error.response.status;
+          let message = 'Wystąpił nieznany błąd.';
+          if (code >= 500) {
+            message = 'Wystąpił błąd serwera, skontaktuj się z administratorem.';
+          }
+          swal({
+            type: 'error',
+            title: 'Błąd',
+            text: message,
+            timer: 5000,
+          });
+        });
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+};
+</script>
